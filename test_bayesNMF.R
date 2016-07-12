90a346efb6c94b89fbc0573ff5fcea565ba9276b
Jacqueline Buros Novik <jackinovik@gmail.com>
2016-07-12 14:08:48 -0400

    recreate summary produced by sample code (using get.df.solution, etc)

--- test_bayesNMF.R
+++ test_bayesNMF.R
@@ -26,7 +26,7 @@ res <- BayesNMF.L1KL(V0 = m, n.iter = n.iter, a0 = a0, tol = tol, K = Kcol, K0 =
 names(res) <- c('W','H','n.like','n.evid','n.lambda','n.error')
 str(res['W'])
 
-## format results 
+## inspect raw 'W' object returned
 results <- 
   as.data.frame(res['W']) %>%
   dplyr::mutate(mutation_type = d$Mutation.Type,
@@ -37,11 +37,44 @@ results <-
                 , mutation_replacement = gsub(mutation_type, pattern = '(\\D)\\[(\\D>\\D)\\](\\D)', replacement = '\\2')
                 )
 
-## plot/summarize signatures
+## plot/summarize 'W'
 ggplot(data = results, aes(x = mutation_context, y = weight, fill = mutation_replacement)) + 
   stat_summary(fun.y = identity, geom = 'bar', position = 'stack') + facet_grid(signature~mutation_replacement) +
   theme_minimal() + theme(axis.text.x=element_text(angle=90, hjust=1))
 
+## identify K
+summarize_k <- function(res) {
+  lambda <- res[['n.lambda']]
+  lambda <- lambda[[length(lambda)]]
+  lambda.min <- min(lambda)
+  lambda <- lambda-lambda.min
+  lambda.norm <- lambda/sum(lambda)
+  index <- lambda.norm > 0.01
+  K <- sum(index)
+  return(K)
+}
+print(summarize_k(res))
+
+## re-create summary produced by example_usage.R code
+summarize_results <- function(res = NULL,
+                              W = res[['W']],
+                              H = res[['H']],
+                              lambda = res[['n.lambda']],
+                              tumor.type = 'dataset'
+) { 
+  W1 <- W
+  H1 <- H
+  W.norm <- apply(W,2,function(x) x/sum(x))
+  for (i in 1:ncol(W)) {
+    H1[i,] <- H1[i,]*colSums(W)[i]
+    W1[,i] <- W1[,i]*rowSums(H)[i]
+  }
+  df <- get.df.solution(W1, H, lambda, tumor.type)
+  K <- length(table(df$class))
+  return(plot.lego.observed.barplot(df,paste("Mutation Signatures in ",tumor.type,sep="")))
+}
+summarize_results(res, tumor.type = 'BLCA') +
+  theme(axis.text.x=element_text(size = 8))
 
 
 
